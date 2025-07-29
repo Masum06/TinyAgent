1bd0ae17aca98aef16ed0378636eed5b5c07cd76
Masum Hasan <youngladesh@gmail.com>
2025-07-29 15:45:12 -0400

    Update tinyagent.py

--- tinyagent.py
+++ tinyagent.py
@@ -6,24 +6,32 @@ os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
 from openai import OpenAI
 client = OpenAI()
 
+import json
+import tiktoken
+
 class TinyAgent:
-  def __init__(self, model="gpt-4o-mini", name="TinyAgent", system=""):
+  def __init__(self, name="", system=""):
     self.name = name
     self.system = system
     self.messages = []
     self.temperature = 1
     self.max_tokens = 2048
-    self.model = model
+    self.model = "gpt-4.1"
     if name:
       self.add_system_message(f"Your name is {name}.")
     if system:
       self.add_system_message(system)
 
-  def set_max_tokens(self, max_tokens):
-    self.max_tokens = max_tokens
+  def token_count(self, string):
+    encoding = tiktoken.encoding_for_model("gpt-4o") # 4.1 not in tiktoken yet
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
 
-  def set_model(self, model):
-    self.model = model
+  def prompt_token_count(self):
+    prompt = ""
+    for message in self.messages:
+      prompt += message["content"]
+    return self.token_count(prompt)
 
   def add_message(self,message_type, message):
     self.messages.append({"role": message_type, "content":message})
@@ -46,16 +54,20 @@ class TinyAgent:
   def set_temperature(self, temperature):
     self.temperature = temperature
 
+  def set_max_tokens(self, max_tokens):
+    self.max_tokens = max_tokens
+
+  def set_model(self, model):
+    self.model = model
 
   def call(self, prompt="", response_type="text"):
-    messages = self.messages 
     if prompt:
-      messages.append({"role": "user", "content": prompt})
+      self.add_user_message(prompt)
     response = client.chat.completions.create(
       model=self.model,
-      messages=messages,
+      messages=self.messages,
       temperature=self.temperature,
-      max_tokens=self.max_tokens,
+      max_tokens=max(self.max_tokens, int(self.prompt_token_count()*2)),
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0,
@@ -76,10 +88,17 @@ class TinyAgent:
         return None
 
   def call_json(self, prompt=""):
+    prompt += "\n\nOutput must be JSON format.\n\n"
     reply = self.call(prompt, response_type="json_object")
-    reply_json = self.load_json(reply)
-    return reply_json
+    try:
+      reply_json = self.load_json(reply)
+      if reply_json[-1] != "}":
+        raise Exception("Incomplete JSON")
+    except:
+      reply_json = None
+      print("Error parsing JSON")
 
+    return reply_json
 
 ## Create a TinyAgent
 tiny_agent = TinyAgent("tiny_agent 🤖", "You perform math.")
